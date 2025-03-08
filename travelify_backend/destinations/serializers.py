3a1904952d074da77e2af52f4eb557f0c77fed6f
aarya-ghimire <aaryaghimire36@gmail.com>
2025-03-08 10:38:45 +0545

    new commit

--- travelify_backend/destinations/serializers.py
+++ travelify_backend/destinations/serializers.py
@@ -8,15 +8,22 @@ class DestinationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Destination
         fields = ['id', 'name', 'description', 'category', 'best_time', 'image_url', 'created_at', 'category_data']
-    
+
     def get_category_data(self, obj):
         return {"id": obj.category.id, "name": obj.category.name}
 
     def create(self, validated_data):
         category_name = validated_data.pop('category', None)
-
         if category_name:
             category, created = Category.objects.get_or_create(name=category_name)
             validated_data['category'] = category
-
         return Destination.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):  # ✅ Add this update method
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.best_time = validated_data.get('best_time', instance.best_time)
+        instance.image_url = validated_data.get('image_url', instance.image_url)
+        instance.category = validated_data.get('category', instance.category)
+        instance.save()
+        return instance
--- travelify_backend/reviews/views.py
+++ travelify_backend/reviews/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Review
+from .serializers import ReviewSerializer
+from rest_framework.decorators import action
 
-# Create your views here.
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    # Retrieve Reviews for a Specific Destination
+    @action(detail=False, methods=['get'], url_path='destination/(?P<destination_id>[^/.]+)')
+    def get_reviews_for_destination(self, request, destination_id=None):
+        reviews = Review.objects.filter(destination__id=destination_id)
+        serializer = self.get_serializer(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Retrieve Reviews for a Specific User
+    @action(detail=False, methods=['get'], url_path='user/(?P<user_id>[^/.]+)')
+    def get_reviews_by_user(self, request, user_id=None):
+        reviews = Review.objects.filter(user__id=user_id)
+        serializer = self.get_serializer(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
